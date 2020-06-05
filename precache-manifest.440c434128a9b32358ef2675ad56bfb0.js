af7745e432adb8cdc4cde5c7720d05990dd415fd
vanshika <vanshudhankani99@gmail.com>
2020-06-05 20:48:35 +0530

    Updates

---
+++ precache-manifest.440c434128a9b32358ef2675ad56bfb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48712ab956ac5ed175452a0193fd37b0",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bdd9ff9871d17ee9857f",
+    "url": "/robofriends/static/css/main.01b0b218.chunk.css"
+  },
+  {
+    "revision": "c016c2724503137269b8",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.88b08eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd9ff9871d17ee9857f",
+    "url": "/robofriends/static/js/main.656d89a6.chunk.js"
+  },
+  {
+    "revision": "99557a09ab68caec6407",
+    "url": "/robofriends/static/js/runtime-main.229ccf3b.js"
+  }
+]);
